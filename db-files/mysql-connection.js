b1a7adbcae8a49e06e06fe2584db7e2286cd3700
mata998 <mateja.ivanovic1998@gmail.com>
2021-10-18 01:48:26 +0200

    db secrets fixed

--- db-files/mysql-connection.js
+++ db-files/mysql-connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: 3306,
-  database: "baza",
-  user: "root",
-  password: "lozinka123",
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 async function pingDB() {
