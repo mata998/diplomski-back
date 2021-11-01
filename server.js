89ddcf2994b197cb249f0f5fd7f1372ea83135db
mata998 <mateja.ivanovic1998@gmail.com>
2021-11-01 19:28:20 +0100

    docker goes brrr

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 // Requires
-const path = require("path");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
@@ -15,12 +14,15 @@ const { userFromTokenMid, isAdminMid } = require("./middlewares/middlewares");
 
 // Cors options
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:5000"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://35.198.186.251:5000",
+  ],
   credentials: true,
 };
 
 // Middlewares
-app.use(express.static(path.join(__dirname, "build")));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(fileUpload());
@@ -30,12 +32,12 @@ app.use("/api/courses", coursesRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/admin", userFromTokenMid, isAdminMid, adminRouter);
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log(`Listening on port 3000!`);
 });
 
 app.get("/test", async (req, res) => {
   console.log("Test route");
 
-  res.json({ msg: "aa" });
+  res.json({ msg: "test aa" });
 });
